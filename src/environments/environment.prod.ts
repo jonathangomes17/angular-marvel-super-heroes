435382df82ffe1d9775e72ef0c0741b9ac8558cb
Jonathan Gomes <jonathangomes17@gmail.com>
2022-01-29 12:30:22 -0300

    added prettier + eslintrc and fixes html/scss/ts codes linted style

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+
+  marvelEndpoint: 'https://gateway.marvel.com/v1/public/',
+  marvelEndpointHash: '72e5ed53d1398abb831c3ceec263f18b',
+  marvelEndpointApiKey: '001ac6c73378bbfff488a36141458af2',
+  marvelEndpointTimestamp: 'thesoer'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: false,
+
+  marvelEndpoint: 'https://gateway.marvel.com/v1/public/',
+  marvelEndpointHash: '72e5ed53d1398abb831c3ceec263f18b',
+  marvelEndpointApiKey: '001ac6c73378bbfff488a36141458af2',
+  marvelEndpointTimestamp: 'thesoer'
 };
 
 import 'zone.js/plugins/zone-error';
